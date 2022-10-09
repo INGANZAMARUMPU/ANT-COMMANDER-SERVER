f4158a901095b3bb779503313cce152910b4c369
INGANZAMARUMPU <jonkur96@gmail.com>
2022-10-09 15:33:55 +0200

    added consumer and template

--- ANT_COMMANDER/settings.py
+++ ANT_COMMANDER/settings.py
@@ -44,7 +44,7 @@ ROOT_URLCONF = 'ANT_COMMANDER.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -108,6 +108,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 STATIC_URL = '/static/'
 
 # Default primary key field type
