f482db718553eb52954de4fe3382d599979bb414
INGANZAMARUMPU <jonkur96@gmail.com>
2022-11-25 11:29:56 +0200

    join room mecanism

--- app/consumers.py
+++ app/consumers.py
@@ -6,6 +6,9 @@ from asgiref.sync import async_to_sync
 channel_layer = get_channel_layer()
 
 class ChatConsumer(WebsocketConsumer):
+    offer = None
+    candidate = []
+
     def connect(self):
         async_to_sync(channel_layer.group_add)("global", self.channel_name)
         self.accept()
@@ -14,11 +17,24 @@ class ChatConsumer(WebsocketConsumer):
         pass
 
     def receive(self, text_data):
-        
         async_to_sync(channel_layer.group_send)('global', {
         	"type": "global.event",
             'message': text_data
         })
 
     def global_event(self, event):
-        self.send(text_data=event["message"])
+        str_message = event["message"]
+        message = json.loads(str_message)
+
+        if(message["order"] == "set-offer"):
+            self.offer = str_message
+            self. candidates = []
+        elif(message["order"] == "remote-candidate"):
+            self.candidates.append(str_message)
+        elif(message["order"] == "join-room"):
+            self.send(text_data=offer)
+            for candidate in self.candidates:
+                self.send(text_data=candidate)
+            return
+
+        self.send(text_data=str_message)
