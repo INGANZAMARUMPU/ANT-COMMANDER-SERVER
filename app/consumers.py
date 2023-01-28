9fc62bde60d99c3d9e8e29b9a0d08b692a73f538
INGANZAMARUMPU <jonkur96@gmail.com>
2023-01-28 11:43:09 +0200

    resend personal GROUP on specific endpoint

--- app/consumers.py
+++ app/consumers.py
@@ -6,46 +6,47 @@ channel_layer = get_channel_layer()
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        await channel_layer.group_add("global", self.channel_name)
+        channel = self.channel_name.split("!")[1]
+        room = self.scope['url_route']["kwargs"]["room_name"]
+        
+        await channel_layer.group_add(room, self.channel_name)
+        await channel_layer.group_add(channel, self.channel_name)
         await self.accept()
+        
+        await channel_layer.group_send(channel, {
+            "type": "global.event",
+            'message': json.dumps({
+                "order": "your_id",
+                "message": channel
+            })
+        })
 
     async def disconnect(self, close_code):
-        channel = self.channel_name
-        room = self.scope['url_route']["kwargs"]["room_name"]
-        print(f"[DISCONNECTING] {channel=}")
-        print(f"[DISCONNECTING] {room=}")
         pass
 
     async def receive(self, text_data):
         room = self.scope['url_route']["kwargs"]["room_name"]
         data = json.loads(text_data)
         order = data["order"]
-
+        print(f"[NEW MESSAGE] {data}")
         if(order == "new_robot"):
-            await channel_layer.group_add(
-                data["message"]["id"],
-                self.channel_name
-            )
-            return channel_layer.group_send('global', {
+            return channel_layer.group_send('commander', {
                 "type": "global.event",
                 'message': text_data
             })
-        elif(order == "new_robot"):
-            return channel_layer.group_send('global', {
+        elif(order == "new_commander"):
+            return channel_layer.group_send('robot', {
                 "type": "global.event",
                 'message': text_data
             })
+
         destination = data.get("dest")
         if(destination):
-            return channel_layer.group_send('global', {
+            return channel_layer.group_send(destination, {
                 "type": "global.event",
-                "room_id": destination,
                 'message': text_data
             })
-        return channel_layer.group_send('global', {
-            "type": "global.event",
-            'message': text_data
-        })
 
     async def global_event(self, event):
-        await self.send(text_data=event["message"])
+        print(f"[SENDING] {event['message']}")
+        await self.send(text_data=event['message'])
